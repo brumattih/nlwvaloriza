96556553b8b3467f1110ff3b57de5de250026822
Henrique Brumatti <hrpreis@gmail.com>
2021-06-21 22:10:11 -0300

    sets up createusers migrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
-import express, { response } from 'express';
+import 'reflect-metadata';
+import express from 'express';
+
+import './database';
 
 const app = express();
-
-app.get("/test", (request, response) => {
-    return response.send("Olá!!");
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá método post");
-});
 
 app.listen(3000, () => console.log("Server is running"));
