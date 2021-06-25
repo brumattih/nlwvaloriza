a6fbd392b608fd4c5cdf12bb868545724c08605e
Henrique Brumatti <hrpreis@gmail.com>
2021-06-24 22:21:49 -0300

    add class-transformer to create a custom column on tag entity

--- src/services/ListTagsService.ts
+++ src/services/ListTagsService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from "typeorm";
 import { TagRepositories } from "../repositories/TagRepositories";
+import { classToPlain } from "class-transformer";
 
 class ListTagsService {
     async execute() {
@@ -7,7 +8,7 @@ class ListTagsService {
 
         const tags = await tagRepositories.find();
 
-        return tags
+        return classToPlain(tags);
     }
 }
 
