dc783e47d0d61c61e110d00082c1c0110cb673d2
Henrique Brumatti <hrpreis@gmail.com>
2021-06-24 22:09:16 -0300

    add list-tags route

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { AuthenticateUserController } from './controllers/AuthenticateUserContro
 import { CreateComplimentController } from './controllers/CreateComplimentController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
+import { ListTagsController } from './controllers/ListTagsController';
 import { ListUserComplimentsReceivedController } from './controllers/ListUserComplimentsReceivedController';
 import { ListUserComplimentsSentController } from './controllers/ListUserComplimentsSentController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
@@ -16,14 +17,16 @@ const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
 const listUserComplimentsSentController = new ListUserComplimentsSentController();
 const listUserComplimentsReceivedController = new ListUserComplimentsReceivedController();
+const listTagsController = new ListTagsController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
 
-router.get("/users/compliments/sent", ensureAuthenticated, listUserComplimentsSentController.handle)
-router.get("/users/compliments/received", ensureAuthenticated, listUserComplimentsReceivedController.handle)
+router.get("/users/compliments/sent", ensureAuthenticated, listUserComplimentsSentController.handle);
+router.get("/users/compliments/received", ensureAuthenticated, listUserComplimentsReceivedController.handle);
 
+router.get("/tags", ensureAuthenticated, listTagsController.handle);
 
 export { router };
