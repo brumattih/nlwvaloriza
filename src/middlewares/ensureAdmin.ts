4b31a6f7c9eab0073fa58666311b25707fd99bc8
Henrique Brumatti <hrpreis@gmail.com>
2021-06-24 21:29:04 -0300

    fix ensureadmin middleware

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,7 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UserRepositories } from '../repositories/UserRepositories';
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-    const admin = true;
+export async function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+    const { user_id } = request;
+    console.log(user_id);
+
+    const userRepositories = getCustomRepository(UserRepositories);
+
+    const { admin } = await userRepositories.findOne(user_id);
 
     if (admin) {
         return next();
